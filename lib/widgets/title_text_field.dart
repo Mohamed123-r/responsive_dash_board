5b3b2b4b9bd3778cce897708453c548f361c9595
mohamed <mezoattalla123@gmail.com>
2024-06-24 21:40:07 +0300

    Add TitleTextField widget with title and hint text support for enhanced form input labeling.

---
+++ lib/widgets/title_text_field.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import '../utils/app_styles.dart';
+import 'custom_text-field.dart';
+
+class TitleTextField extends StatelessWidget {
+  const TitleTextField({super.key, required this.title, required this.hintText});
+final String title ,hintText;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: AppStyles.styleMedium16(),
+        ),
+        const SizedBox(
+          height: 12,
+        ),
+         CustomTextField(hintText: hintText,),
+      ],
+    );
+  }
+}
