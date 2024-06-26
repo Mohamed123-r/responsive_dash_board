bb06dca56d7b26500c983bcb5bed0fae67717892
mohamed <mezoattalla123@gmail.com>
2024-06-26 12:26:20 +0300

    Add MyCardPageView widget with ExpandablePageView integration and update MyCard to use MyCardPageView. Add expandable_page_view dependency to pubspec.yaml.

--- lib/widgets/my_card.dart
+++ lib/widgets/my_card.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../utils/app_styles.dart';
 import 'CustomCard.dart';
 import 'custom_background_container.dart';
+import 'my_card_page_view.dart';
 
 class MyCard extends StatelessWidget {
   const MyCard({super.key});
@@ -19,7 +20,7 @@ class MyCard extends StatelessWidget {
           const SizedBox(
             height: 20,
           ),
-          const CustomCard(),
+          const MyCardPageView(),
         ],
       ),
     );
