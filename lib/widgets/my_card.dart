2ad0a677e8dc2a38b6d07ef91dc0979fa5a03ba4
mohamed <mezoattalla123@gmail.com>
2024-06-26 15:39:03 +0300

    Add TransactionHistory widget to MyCard with Divider for transaction display.

--- lib/widgets/my_card.dart
+++ lib/widgets/my_card.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'custom_background_container.dart';
 import 'my_card_section.dart';
+import 'transaction_history.dart';
+
 
 class MyCard extends StatelessWidget {
   const MyCard({super.key});
@@ -12,9 +14,13 @@ class MyCard extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           MyCardSection(),
+          Divider(
+            height: 40,
+            color: Color(0xffF1F1F1),
+          ),
+          TransactionHistory(),
         ],
       ),
     );
   }
 }
-
---
+++ lib/widgets/transaction_history.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/transation_history_header.dart';
+
+import '../utils/app_styles.dart';
+
+class TransactionHistory extends StatelessWidget {
+  const TransactionHistory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const TransactionHistoryHeader(),
+        const SizedBox(
+          height: 20,
+        ),
+        Text(
+          "13 April 2022",
+          style: AppStyles.styleMedium16().copyWith(
+            color: const Color(0xffAAAAAA),
+          ),
+        ),
+
+      ],
+    );
+  }
+}
