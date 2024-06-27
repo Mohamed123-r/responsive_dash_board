c5da5938eec6d65edde196da65679007fe47f0c7
mohamed <mezoattalla123@gmail.com>
2024-06-27 14:37:41 +0300

    Add IncomeHeader and Income widgets with CustomBackGroundContainer integration for income section display.

---
+++ lib/widgets/income.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'custom_background_container.dart';
+import 'income_header.dart';
+
+class Income extends StatelessWidget {
+  const Income({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CustomBackGroundContainer(
+      child: Column(
+        children: [
+          IncomeHeader(),
+        ],
+      ),
+    );
+  }
+}
+
+
