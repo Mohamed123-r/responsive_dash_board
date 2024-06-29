1f08572bbd5e05d0d370a464978dc61470e8a677
mohamed <mezoattalla123@gmail.com>
2024-06-29 14:34:57 +0300

    Add CustomDrawer to DashBoardView for mobile layout with responsive AppBar integration

--- lib/views/dash_board_view.dart
+++ lib/views/dash_board_view.dart
@@ -4,21 +4,27 @@ import 'package:responsive_dash_board/widgets/dash_board_desktop_layout.dart';
 import 'package:responsive_dash_board/widgets/dash_board_mobile_layout.dart';
 import 'package:responsive_dash_board/widgets/dash_board_tablet.dart';
 
+import '../widgets/custom_drawer.dart';
+
 class DashBoardView extends StatelessWidget {
   const DashBoardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return
-      Scaffold(
-        backgroundColor: Color(0xffF7F9FA),
-        body: AdaptiveLayout(
-          mobileLayout: (context) => const DashBoardMobileLayout(),
-          tabletLayout: (context) => const DashBoardTablet(),
-          desktopLayout: (context) => const DashBoardDesktopLayout(),
-        ),
-      );
-
-
+    return Scaffold(
+      drawer:
+          MediaQuery.of(context).size.width < 800 ? const CustomDrawer() : null,
+      appBar: MediaQuery.of(context).size.width < 800
+          ? AppBar(
+              backgroundColor: const Color(0xff4EB7F2),
+            )
+          : null,
+      backgroundColor: const Color(0xffF7F9FA),
+      body: AdaptiveLayout(
+        mobileLayout: (context) => const DashBoardMobileLayout(),
+        tabletLayout: (context) => const DashBoardTablet(),
+        desktopLayout: (context) => const DashBoardDesktopLayout(),
+      ),
+    );
   }
 }
