dfd485d19a6f10b29cc0a6f38b3d4129c9cf7c38
mohamed <mezoattalla123@gmail.com>
2024-06-29 14:10:21 +0300

    Add DashBoardTablet layout and update AdaptiveLayout breakpoints and DashBoardView to include tablet layout

--- lib/views/dash_board_view.dart
+++ lib/views/dash_board_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/adaptive_layout.dart';
 import 'package:responsive_dash_board/widgets/dash_board_desktop_layout.dart';
+import 'package:responsive_dash_board/widgets/dash_board_tablet.dart';
 
 class DashBoardView extends StatelessWidget {
   const DashBoardView({super.key});
@@ -9,7 +10,7 @@ class DashBoardView extends StatelessWidget {
   Widget build(BuildContext context) {
     return AdaptiveLayout(
       mobileLayout: (context) => const SizedBox(),
-      tabletLayout: (context) => const SizedBox(),
+      tabletLayout: (context) => const DashBoardTablet(),
       desktopLayout: (context) => const DashBoardDesktopLayout(),
     );
   }
