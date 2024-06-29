b43afd71e97f4ae14c7ad2bc71a331bdf2df437a
mohamed <mezoattalla123@gmail.com>
2024-06-29 14:21:16 +0300

    Add DashBoardMobileLayout widget with AllExpansesAndQuickInvoiceSection and MyCardAndIncomeSection integration

---
+++ lib/widgets/dash_board_mobile_layout.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'all_expanses_end_quick_invoice_section.dart';
+import 'my_card_and_income_section.dart';
+
+class DashBoardMobileLayout extends StatelessWidget {
+  const DashBoardMobileLayout({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            AllExpansesAndQuickInvoiceSection(),
+            MyCardAndIncomeSection(),
+          ],
+        ),
+      ),
+    );
+  }
+}
